0d520fa1cf8cc43b419a721f88c6affdbbc697d1
nicolesue <nicole.sue@mail.mcgill.ca>
2021-12-17 03:12:18 -0500

    testing local audio file

--- final_project/waveform.js
+++ final_project/waveform.js
@@ -191,8 +191,8 @@ document.addEventListener("DOMContentLoaded", function() {
     //     }
     // })
     // wavesurfer.load(audio);
-    wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
-    // wavesurfer.load('chanel.mp3');
+    // wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+    wavesurfer.load('chanel.mp3');
     wavesurfer.setPlaybackRate(playback);
 
     // play button
